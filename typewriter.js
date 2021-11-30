5a9a96cfc5c925ff5a1edee7d1f4b0a4180497e1
Mikko De Los Reyes <mikkode212@gmail.com>
2021-11-30 19:57:35 +0000

    typewriter

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs\n";
+let delay = 0;
+const increment = 400;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char)
+  }, delay);
+  delay += increment;
+  
+}
